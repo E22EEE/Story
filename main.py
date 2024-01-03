52c3a7483e6bc8957d48e0ce83cf6fc48026d69e
raon <91876178+E22EEE@users.noreply.github.com>
2024-01-03 17:53:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,71 @@
 from config import Config
-import telebot
-import requests
-tokzz=Config.TG_BOT_TOKEN
-bot = telebot.TeleBot("tokzz")
+from pyrogram import Client, filters
 
+app = Client('my_acount',api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,"")
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "ارسل ال ip.")
+"""***---******---******---******---******---******---******---******---******---******---***"""
 
-
-
-@bot.message_handler(func=lambda message: True)
-def ip_lookup(message):
-    try:
-        ip_address = message.text
-        response = requests.get("http://ip-api.com/json/" + ip_address)
-
-        if response.status_code == 200:
-            data = response.json()
-            output = "IP Address: " + data["query"] + "\n"
-            output += "الحالة: " + data["status"] + "\n"
-            output += "القارة: " + data.get("continent", "N/A") + "\n"
-            output += "كود القارة: " + data.get("continentCode", "N/A") + "\n"
-            output += "الدولة: " + data["country"] + "\n"
-            output += "كود الدولة: " + data["countryCode"] + "\n"
-            output += "منطقة: " + data["region"] + "\n"
-            output += "اسم المنطقة: " + data["regionName"] + "\n"
-            output += "المدينة: " + data["city"] + "\n"
-            output += "يصرف: " + data.get("district", "N/A") + "\n"
-            output += "رمز بريدي: " + data["zip"] + "\n"
-            output += "خط العرض: " + str(data["lat"]) + "\n"
-            output += "خط الطول: " + str(data["lon"]) + "\n"
-            output += "الوحدة الزمنية: " + data["timezone"] + "\n"
-            output += "عوض: " + str(data.get("offset", "N/A")) + "\n"
-            output += "عملة: " + data.get("currency", "N/A") + "\n"
-            output += "ISP: " + data["isp"] + "\n"
-            output += "منضمة: " + data["org"] + "\n"
-            output += "AS: " + data["as"] + "\n"
-            output += "AS Name: " + data.get("asname", "N/A") + "\n"
-            output += "Mobile: " + str(data.get("mobile", "N/A")) + "\n"
-            output += "Proxy: " + str(data.get("proxy", "N/A")) + "\n"
-            output += "Hosting: " + str(data.get("hosting", "N/A")) + "\n"
-
-            bot.reply_to(message, output)
-        else:
-            bot.reply_to(message, "Failed to get data from API")
-    except:
-        bot.reply_to(message, "An error occurred. Please try again.")
-
-
-
-
-bot.polling()
+id = 5154904380
+ch = -1001198143769
+yes = ["no"]
+"""***---******---******---******---******---******---******---******---******---******---***"""
+#هنا اوامر التفعيل والتعطيل#
+"""***---******---******---******---******---******---******---******---******---******---***"""
+@app.on_message(filters.command(["تعطيل","عطل"],prefixes=f""))
+async def yas(yess,message):
+	if "no" in yes:
+		await app.send_message(message.chat.id,"معطل")
+	else:
+		yes[0] = "no"
+		await app.send_message(message.chat.id,"تم التعطيل")
+@app.on_message(filters.command(["تفعيل","فعل"],prefixes=f""))
+async def No(NO,message):
+	if "yes" in yes:
+		await app.send_message(message.chat.id,"مفعل")
+	else:
+		yes[0] = "yes"
+		await app.send_message(message.chat.id,"تم التفعيل")
+"""***---******---******---******---******---******---******---******---******---******---***"""	#هنا اخذ المنشورات#
+"""***---******---******---******---******---******---******---******---******---******---***"""	
+@app.on_message(filters.chat(int(ch)) & filters.all)
+async def handle_messages(client, message):
+	if "yes" in yes:
+		if message.text:
+			if "@aeo_n" in message.text:
+				msg =message.text.replace("@aeo_n","@iqrels")
+				ci = await app.get_chat(message.chat.id)
+				msg = message.text
+				title = ci.title
+				username = ci.username
+				await app.send_message(id,f"""رسالة جديدة من قناة : {title}
+يوزر القناة : @{username}
+الرسالة : {msg}""")
+			else:
+				ci = await app.get_chat(message.chat.id)
+				msg = message.text
+				title = ci.title
+				username = ci.username
+				await app.send_message(id,f"""رسالة جديدة من قناة : {title}
+يوزر القناة : @{username}
+الرسالة : {msg}""")
+		if message.video:
+			if "@aeo_n" in message.caption:
+				msg = message.caption.replace("@aeo_n","@iqrels")
+				file = message.video.file_id
+				await app.send_video(id,video=message.video.file_id,caption=msg)
+			else:
+				file = message.video.file_id
+				await app.send_video(id,video=file,caption=msg)
+		if message.photo:
+			if "@aeo_n" in message.caption:
+				msg = message.caption.replace("@aeo_n","@iqrels")
+				file = message.photo.file_id
+				await app.send_photo(id,photo=file,caption=msg)
+			else:
+				msg = message.caption
+				file = message.photo.file_id
+				await app.send_photo(id,photo=file,caption=msg)
+"""***---******---******---******---******---******---******---******---******---******---***"""	
+print("run the bot")
+app.run()
